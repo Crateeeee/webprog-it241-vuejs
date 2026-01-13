a00d6dd749bca87a40e21e9bcaca551a6603d343
Crateeeee <capinlac@student.apc.edu.ph>
2026-01-13 01:15:07 +0000

    adobo

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -3,38 +3,16 @@
   <food-item/>
   <food-item/>
   <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
 </template>
 
 <script></script>
 
-<style></style>
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+</style>
--- firstsfc/src/components/FoodItem.vue
+++ firstsfc/src/components/FoodItem.vue
@@ -9,8 +9,8 @@
 export default {
   data() {
     return {
-      name: 'Apples',
-      message: 'I like apples'
+      name: 'Adobo',
+      message: 'Chicken Adobo is way better than Pork Adobo. Fight me!'
     }
   }
 };
