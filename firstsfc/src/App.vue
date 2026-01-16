fa2f6e0d71d86043aed971bc45ccddc858cf661b
Crateeeee <capinlac@student.apc.edu.ph>
2026-01-16 19:45:58 +0800

    update errors

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,20 +1,11 @@
 <template>
+
   <h1>Food</h1>
   <food-item/>
   <food-item2/>
+ 
 </template>
 
-<script>
-import FoodItem from './components/FoodItem.vue'
-import FoodItem2 from './components/FoodItem2.vue'
-
-export default {
-  name: 'App',
-  components: {
-    'food-item': FoodItem,
-    'food-item2': FoodItem2
-  }
-}
-</script>
+<script></script>
 
 <style></style>
