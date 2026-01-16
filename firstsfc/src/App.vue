0fcdcba5607f93a396e1e077896aceb0cd916d37
Crateeeee <capinlac@student.apc.edu.ph>
2026-01-16 08:50:22 +0800

    updated app vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>Fruits</h1>
+  <h1>Food</h1>
   <food-item/>
   <food-item2/>
 </template>
 
-<script></script>
+<script>
+import FoodItem from './components/FoodItem.vue'
+import FoodItem2 from './components/FoodItem2.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'food-item': FoodItem,
+    'food-item2': FoodItem2
+  }
+}
+</script>
 
 <style></style>
