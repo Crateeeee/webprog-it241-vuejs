83abf49bfb0fe0303e3d5746b43b7df0b8063f5c
Crateeeee <capinlac@student.apc.edu.ph>
2026-01-28 22:30:55 +0800

    updated

---
+++ firstsfc/src/components/FoodItem2.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="Chicken">
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Chicken',
+      message: 'I like Chicken'
+    }
+  }
+};
+</script>
+
+<style>
+.Chicken {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: orange;
+  }
+</style>
